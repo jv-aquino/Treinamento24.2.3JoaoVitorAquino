5ace6a03e8957aaaffc39258b59c8913c5283583
João Vitor de Aquino <jvaquinocontact@gmail.com>
2024-11-10 17:15:48 -0300

    + fix news scroll

--- components/News/News.tsx
+++ components/News/News.tsx
@@ -1,22 +1,23 @@
 import { baseNews } from "@/public/data/baseNews";
-
 import Image from "next/image";
 import NewsCard from "./NewsCard";
 import { v4 as uuidv4 } from 'uuid';
 
 function News() {
   return ( 
-    <div className="w-[360px] flex-shrink-0 overflow-y-auto h-screen px-4 py-8 no-scrollbar">
-      <h2 className="flex text-headline font-bold pb-6">
+    <div className="w-[340px] flex-shrink-0 h-screen flex flex-col px-4 py-8">
+      <h2 className="flex items-center text-headline font-bold pb-6 flex-shrink-0">
         <Image src="/assets/icons/newsIcon.svg" alt='' width={32} height={32} /> 
         Pius em alta
       </h2>
 
-      {baseNews.map(noticia => {
-        return <NewsCard noticia={noticia} key={uuidv4()} />
-      })}
+      <div className="overflow-y-auto no-scrollbar flex-1">
+        {baseNews.map(noticia => (
+          <NewsCard noticia={noticia} key={uuidv4()} />
+        ))}
+      </div>
     </div>
-   );
+  );
 }
 
 export default News;
